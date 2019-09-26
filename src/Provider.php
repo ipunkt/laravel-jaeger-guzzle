8289d4362606c43a55df504d83328bd238fa49b9
svensp <sps@ipunkt.biz>
2019-09-26 14:53:58 +0200

    initial version

---
+++ src/Provider.php
@@ -0,0 +1,18 @@
+<?php namespace Ipunkt\LaravelJaegerGuzzle;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class Provider
+ * @package Ipunkt\LaravelJaeger
+ */
+class Provider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php namespace Ipunkt\LaravelJaegerGuzzleTests;
+
+/**
+ * Class TestCase
+ * @package Ipunkt\LaravelJaegerTests
+ */
+class TestCase extends \Orchestra\Testbench\TestCase {
+
+}
